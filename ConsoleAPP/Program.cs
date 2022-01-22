65426bba41daea672f55a38668d0cd95ed551c76
martinskwy <martinskwy@outlook.com>
2022-01-22 12:03:30 -0300

    first commit

---
+++ ConsoleAPP/Program.cs
@@ -0,0 +1,9 @@
+﻿Knight arus = new Knight("Arus", 23, "Knight");
+Ninja wedge = new Ninja("Wedge", 42, "Ninja");
+Wizard jennica = new Wizard("Jennica", 23, "White Wizard");
+BlackWizard topapa = new BlackWizard("Topapa", 42, "Black Wizard");
+
+Console.WriteLine(arus.Attack());
+Console.WriteLine(wedge.Attack());
+Console.WriteLine(jennica.Attack());
+Console.WriteLine(topapa.Attack());
